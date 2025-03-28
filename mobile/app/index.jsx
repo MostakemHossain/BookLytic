110e8ef9d70d8ed241da265d1e8e82ca61a289ed
MostakemHossain <mostakememon123@gmail.com>
2025-03-28 17:08:18 +0600

    root layout setup

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -11,6 +12,9 @@ export default function Index() {
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Text>This is the default screen.</Text>
+      <Link  href={"/(auth)/signup"}>
+      SignUp
+      </Link>
     </View>
   );
 }
