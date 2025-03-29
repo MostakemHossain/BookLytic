78501d605230274627003075dc83bae55e05e58d
MostakemHossain <mostakememon123@gmail.com>
2025-03-29 08:36:36 +0600

    some changes

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,13 +1,21 @@
 import mongoose from "mongoose";
 import config from "../config/index.js";
 
-export const connectDb=async()=>{
+export const connectDb = async () => {
     try {
-        await mongoose.connect(config.database_url);
-        console.log("MongoDB connected");
+        await mongoose.connect(config.database_url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log("✅ MongoDB connected successfully");
+        mongoose.connection.on("error", (err) => {
+            console.error("❌ MongoDB Connection Error:", err);
+            process.exit(1);
+        });
+
     } catch (error) {
-        console.error(`Error connecting to MongoDB: ${error.message}`);
+        console.error(`❌ Error connecting to MongoDB: ${error.message}`);
         process.exit(1);
-        
     }
-}
+};
