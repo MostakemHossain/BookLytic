1e99608a41b06cb35256a43d33bab41f18b78062
MostakemHossain <mostakememon123@gmail.com>
2025-03-29 10:29:15 +0600

    auto navigation and tabs setup

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,19 +1,42 @@
-import {Stack} from "expo-router"
-import {SafeAreaProvider} from "react-native-safe-area-context";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
-import {StatusBar} from "expo-status-bar"
+import { StatusBar } from "expo-status-bar";
+import {useAuthStore} from "../store/authStore"
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  return <SafeAreaProvider>
-    <SafeScreen>
-    <Stack screenOptions={{
-    headerShown: false,
-  }} >
-    {/* Add your screens here */}
-    <Stack.Screen name="index" />
-    <Stack.Screen name="(auth)"  />
-  </Stack>
-    </SafeScreen>
-    <StatusBar barStyle="dark-content" />
-  </SafeAreaProvider>
+  const router = useRouter();
+  const segments = useSegments();
+  const { checkAuth,token,user}= useAuthStore();
+
+  useEffect(()=>{
+    checkAuth();
+  },[])
+
+  // handle navigation based on the  auth state
+  useEffect(()=>{
+    const isAuthScreen= segments[0]==="(auth)";
+    const isSignedIn= user && token;
+    if(isAuthScreen &&!isSignedIn){
+      router.replace("/(auth)");
+    }else if(isAuthScreen && isSignedIn){
+      router.replace("/(tabs)");
+    }
+  },
+  [token,user,segments]);
+
+  
+  return (
+    <SafeAreaProvider>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }}>
+          {/* Add your screens here */}
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="(auth)" />
+        </Stack>
+      </SafeScreen>
+      <StatusBar barStyle="dark-content" />
+    </SafeAreaProvider>
+  );
 }
